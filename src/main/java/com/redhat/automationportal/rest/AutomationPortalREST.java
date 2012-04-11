179db85f383e043b316dfbee6b78409829935e7f
Matthew Casperson <mcaspers@redhat.com>
2012-04-12 08:29:15 +1000

    Test REST service running ok on JBoss AS 7.1.1

--- src/main/java/com/redhat/automationportal/rest/AutomationPortalREST.java
+++ src/main/java/com/redhat/automationportal/rest/AutomationPortalREST.java
@@ -4,7 +4,7 @@ import java.util.Calendar;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
 @Path("/")
@@ -12,7 +12,7 @@ public class AutomationPortalREST
 {
 	@GET
 	@Path("/BugzillaReportGenerator/get/json")
-	public Response printMessage(@PathParam("bugzillaUsername") final String bugzillaUsername, @PathParam("bugzillaPassword") final String bugzillaPassword) 
+	public Response printMessage(@QueryParam("bugzillaUsername") final String bugzillaUsername, @QueryParam("bugzillaPassword") final String bugzillaPassword) 
 	{
 		final String result = Calendar.getInstance().getTimeInMillis() + ""; 
 		return Response.status(200).entity(result).build();
